0314763090240c0cdd0b689520ee9404f8e04edf
Lucas Machado <lucaspedro.machado@gmail.com>
2016-05-10 17:55:49 -0300

    Carta
    Classe Carta aparentemente pronta. Com exceção, talvez, do ENUM

--- src/Poker/Carta.java
+++ src/Poker/Carta.java
@@ -4,5 +4,25 @@ public class Carta {
 	
 	private int naipe;
 	private int identificador;
+	
+	public Carta(int naipe, int identificador) { //método adicionado//////////////////////////////////////////////////
+		this.naipe = naipe;
+		this.identificador = identificador;
+	}
+	
+	public int getNaipe() { //método adicionado//////////////////////////////////////////////////////////////////////
+		return this.naipe;
+	}
+	
+	public int getIdentificador() { //método adicionado//////////////////////////////////////////////////////////////
+		return this.identificador;
+	}
 
+	public void setNaipe(int naipe) { //método adicionado////////////////////////////////////////////////////////////
+		this.naipe = naipe;
+	}
+	
+	public void setIdentificador(int identificador) { //método adicionado////////////////////////////////////////////
+		this.identificador = identificador;
+	}
 }
